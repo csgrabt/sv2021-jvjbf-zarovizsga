95eabb24c5b7f81493e7b65975efc63cdadb5005
Csekő György <gy.cseko@outlook.com>
2021-08-09 14:28:12 +0200

    Player entitás létrehozása

---
+++ src/main/java/org/training360/finalexam/player/Player.java
@@ -0,0 +1,23 @@
+package org.training360.finalexam.player;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "players")
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Double id;
+    private String name;
+    private LocalDate birthDate;
+    private Position position;
+
+}
